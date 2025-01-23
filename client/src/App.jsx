e43992f9d378eba954f6b16ca430317d555a3811
ksytwu100 <ksytwu@gmail.com>
2025-01-23 06:07:21 -0800

    Add Appolo-server-express

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,53 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.jsx";
+import SearchBooks from "./pages/SearchBooks.jsx";
+import SavedBooks from "./pages/SavedBooks.jsx";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
-import Navbar from './components/Navbar';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <SearchBooks />,
+      },
+      {
+        path: "/saved",
+        element: <SavedBooks />,
+      },
+    ],
+  },
+]);
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <RouterProvider router={router} />
+  </ApolloProvider>
+);
